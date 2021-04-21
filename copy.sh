6b1d15ae8e1900181dce2108699df3531fdd11ef
Sting Alleman <stingalleman@icloud.com>
2021-04-21 17:49:13 +0200

    add duplication script

--- copy.sh
+++ copy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 i=0
-while (( i++ < 4572 )); do
-    cp -r packages/testpkg "packages/testpkg$i"
+while (( i++ < 5000 )); do
+    NAME=$(LC_CTYPE=C tr -dc A-Za-z0-9 < /dev/random | head -c 10 )
+    cp -r packages/testpkg "packages/$NAME"
 done
